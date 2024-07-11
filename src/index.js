b6c85d97c645a98bee66dc42e70235887d7e3e32
ericonp <ericonp@gmail.com>
2024-07-11 15:44:31 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import messages_en from "./translations/pt_BR.json";
+import messages_pt_BR from "./translations/pt_BR.json";
 
 const DEFAULT_CONFIG = {
   "translations": [{ key: "pt_BR", messages: messages_pt_BR }],
 }
 
-export const TemplateModule = (cfg) => {
+export const LanguagePt_BRModule = (cfg) => {
   return { ...DEFAULT_CONFIG, ...cfg };
 }
