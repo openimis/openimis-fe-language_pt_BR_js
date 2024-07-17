211e0e8b416561959b76f8ba8ceff4bd21122a15
Claudio Ferreira Filho <filhocf@gmail.com>
2024-07-17 09:48:15 -0300

    Small adjust in pt-br package

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import messages_pt_br from "./translations/pt-br.json";
 
 const DEFAULT_CONFIG = {
-  "translations": [{ key: "pt-br", messages: messages_pt_br }],
+  "translations": [{ key: "pt", messages: messages_pt_br }],
 }
 
 export const LanguagePtBrModule = (cfg) => {
